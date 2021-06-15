0de81c76e4161f0ef3e9ecb03cb787c442537ac3
TaraCharter <57436591+TaraCharter@users.noreply.github.com>
2021-06-15 17:18:05 -0500

    Updating the code to work with S3 bucket
    Updates to the code so that it works with both S3 bucket and remote code execution (for Dataiku Online) and local filesystem.

--- shared-code/to_xlsx.py
+++ shared-code/to_xlsx.py
@@ -1,11 +1,22 @@
 import dataiku
 import pandas as pd
 import openpyxl
+import io
+import pickle
 
 def dataframe_to_xlsx(input_dataframe, folder_name, output_file_name):
     folder = dataiku.Folder(folder_name)
-    folder_path = folder.get_path()
-    folder_path = folder_path + '/' + output_file_name + '.xlsx'
-    writer = pd.ExcelWriter(folder_path, engine='openpyxl')
-    input_dataframe.to_excel(writer, index=False, encoding='utf-8')
-    writer.save()
+    folder_infos = folder.get_info()
+    if folder_infos["type"] == "S3":
+        pickle_bytes = io.BytesIO()
+        pickle.dump(input_dataframe, pickle_bytes)
+        with folder.get_writer("input_dataframe.p") as w:
+            w.write(pickle_bytes.getvalue())
+    else:
+        folder_path = folder.get_path()
+        folder_path = folder_path + '/' + output_file_name + '.xlsx'
+        writer = pd.ExcelWriter(folder_path, engine='openpyxl')
+        input_dataframe.to_excel(writer, index=False, encoding='utf-8')
+        
+        writer.save()
+    
