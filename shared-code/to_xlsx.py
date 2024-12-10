c7b02edb67d0cb668a39b00fe6f803d4676752bc
Sevan <35869501+sevanga@users.noreply.github.com>
2024-12-10 13:54:27 +0100

    update code

--- shared-code/to_xlsx.py
+++ shared-code/to_xlsx.py
@@ -8,10 +8,14 @@ def dataframe_to_xlsx(input_dataframe, folder_name, output_file_name):
     folder = dataiku.Folder(folder_name)
     folder_infos = folder.get_info()
     if folder_infos["type"] == "S3":
-        pickle_bytes = io.BytesIO()
-        pickle.dump(input_dataframe, pickle_bytes)
-        with folder.get_writer("input_dataframe.p") as w:
-            w.write(pickle_bytes.getvalue())
+        file = output_file_name + '.xlsx'
+        xl_bytes = io.BytesIO()
+        
+        with pd.ExcelWriter(xl_bytes, engine='openpyxl') as writer:
+            input_dataframe.to_excel(writer, index=False, encoding='utf-8')
+
+        with folder.get_writer(file) as fwriter:
+            fwriter.write(xl_bytes.getvalue())
     else:
         folder_path = folder.get_path()
         folder_path = folder_path + '/' + output_file_name + '.xlsx'
